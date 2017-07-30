47815a8bed904251f32463fd22179a15f37e185a
Sean Walsh <sean@swalsh.org>
2017-07-30 11:45:15 -0700

    netlify redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const fs = require('fs');
+
+const redirects = require('./redirects');
 
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators;
@@ -54,4 +57,10 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
       });
     }
   });
+};
+
+exports.onPostBuild = (...args) => {
+  const netlifyRedirects = redirects.map(({ oldPath, newPath, httpCode }) => `${oldPath} ${newPath} ${httpCode}`);
+
+  fs.writeFileSync(`${__dirname}/public/_redirects`, netlifyRedirects.join('\n'));
 };
---
+++ redirects.js
@@ -0,0 +1,1 @@
+module.exports = [{ oldPath: '/blog/:year/:month/:date/:slug', newPath: '/blog/:slug', httpCode: 301 }];
