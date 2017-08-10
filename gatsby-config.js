3d1b676d2be0aa8e1a982804051ddb6b0e44237a
Sean Walsh <sean@swalsh.org>
2017-08-09 21:34:31 -0700

    add rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,11 @@
+const siteUrl = 'https://swalsh.org';
+
 module.exports = {
   siteMetadata: {
     title: 'Sean Walsh',
     author: 'Sean Walsh',
+    description: 'Personal blog of Sean Walsh',
+    siteUrl,
     socialLinks: {
       github: 'https://github.com/s992',
       twitter: 'https://twitter.com/theseanwalsh',
@@ -39,6 +43,58 @@ module.exports = {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: 'UA-52344422-1',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        setup({ query: { site, allMarkdownRemark } }) {
+          return Object.assign({}, site.siteMetadata, {
+            allMarkdownRemark,
+            generator: 'GatsbyJS',
+            site_url: siteUrl,
+          });
+        },
+        feeds: [
+          {
+            output: '/rss.xml',
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                const link = `${site.siteMetadata.siteUrl}${edge.node.frontmatter.path}`;
+
+                return {
+                  title: edge.node.frontmatter.title,
+                  description: edge.node.excerpt,
+                  url: link,
+                  guid: link,
+                  date: edge.node.frontmatter.date,
+                };
+              });
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  limit: 1000,
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                  filter: { frontmatter: { published: { eq: true } } }
+                ) {
+                  edges {
+                    node {
+                      id
+                      excerpt
+                      frontmatter {
+                        title
+                        date
+                        path
+                        published
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+          },
+        ],
       },
     },
   ],
