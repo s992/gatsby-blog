6c5003e86e521c58dcb65efef6fef3b8e109531a
Sean Walsh <sean@swalsh.org>
2017-08-12 10:01:21 -0700

    fix navbar expand/collapse

--- src/components/header.js
+++ src/components/header.js
@@ -1,40 +1,67 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import classNames from 'classnames';
 
-export default function Header({ title }) {
-  return (
-    <header>
-      <nav className="navbar navbar-default">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <button
-              type="button"
-              className="navbar-toggle collapsed"
-              data-toggle="collapse"
-              data-target="#main-navbar-collapse"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-            </button>
-            <Link className="navbar-brand" to="/">
-              {title}
-            </Link>
+export default class Header extends React.Component {
+  state = {
+    expanded: false,
+  };
+
+  constructor() {
+    super();
+    this.toggleExpanded = this.toggleExpanded.bind(this);
+    this.collapse = this.collapse.bind(this);
+  }
+
+  toggleExpanded() {
+    this.setState(prevState => ({ ...prevState, expanded: !prevState.expanded }));
+  }
+
+  collapse() {
+    this.setState(prevState => ({ ...prevState, expanded: false }));
+  }
+
+  render() {
+    const { title } = this.props;
+    const { expanded } = this.state;
+
+    return (
+      <header>
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <button
+                type="button"
+                className="navbar-toggle collapsed"
+                aria-expanded="false"
+                onClick={this.toggleExpanded}
+              >
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar" />
+                <span className="icon-bar" />
+                <span className="icon-bar" />
+              </button>
+              <Link className="navbar-brand" to="/">
+                {title}
+              </Link>
+            </div>
+            <div className={classNames('navbar-collapse', { collapse: !expanded })}>
+              <ul className="nav navbar-nav">
+                <li>
+                  <Link to="/blog" onClick={this.collapse}>
+                    Blog
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/about" onClick={this.collapse}>
+                    About
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className="collapse navbar-collapse" id="main-navbar-collapse">
-            <ul className="nav navbar-nav">
-              <li>
-                <Link to="/blog">Blog</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
-  );
+        </nav>
+      </header>
+    );
+  }
 }
