c6ba45a84a8889e5e6685aedea055f49348acd43
Sean Walsh <sean@dronedeploy.com>
2018-08-11 21:41:43 -0700

    just leave me alone, lighthouse

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,22 +5,22 @@ export default function Footer({ socialLinks }) {
     <footer id="site-footer" className="container-fluid">
       <ul className="nav nav-pills pull-right">
         <li>
-          <a target="_blank" href={socialLinks.github}>
+          <a target="_blank" rel="noopener noreferrer" href={socialLinks.github} aria-label="Sean's Github Profile">
             <i className="fa fa-github" />
           </a>
         </li>
         <li>
-          <a target="_blank" href={socialLinks.twitter}>
+          <a target="_blank" rel="noopener noreferrer" href={socialLinks.twitter} aria-label="Sean's Twitter Profile">
             <i className="fa fa-twitter" />
           </a>
         </li>
         <li>
-          <a target="_blank" href={socialLinks.stackOverflow}>
+          <a target="_blank" rel="noopener noreferrer" href={socialLinks.stackOverflow} aria-label="Sean's Stack Overflow Profile">
             <i className="fa fa-stack-overflow" />
           </a>
         </li>
         <li>
-          <a target="_blank" href={socialLinks.linkedin}>
+          <a target="_blank" rel="noopener noreferrer" href={socialLinks.linkedin} aria-label="Sean's LinkedIn Profile">
             <i className="fa fa-linkedin" />
           </a>
         </li>
