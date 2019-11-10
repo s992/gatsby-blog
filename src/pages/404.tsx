4f6559ce9d7e37727e89d2affa12e10b56d18fd9
Sean Walsh <sean@dronedeploy.com>
2019-11-09 16:56:19 -0800

    ok typescripted?

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import Layout from '../components/layout'
 
-export default () => (
-  <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn't exist... the sadness.</p>
-    </div>
-  </Layout>
-)
+export default function NotFound() {
+  return (
+    <Layout>
+      <div>
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&apos;t exist... the sadness.</p>
+      </div>
+    </Layout>
+  )
+}
